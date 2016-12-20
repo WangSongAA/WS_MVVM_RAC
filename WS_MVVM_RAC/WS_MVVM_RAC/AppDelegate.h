284c7e2ed7d1a79d738952ce06f5a5217bb1477c
WangSong <369263230@qq.com>
2016-12-20 10:07:37 +0800

    create project...

---
+++ WS_MVVM_RAC/WS_MVVM_RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WS_MVVM_RAC
+//
+//  Created by ws on 16/12/20.
+//  Copyright © 2016年 王松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WS_MVVM_RAC/WS_MVVM_RAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WS_MVVM_RAC
+//
+//  Created by ws on 16/12/20.
+//  Copyright © 2016年 王松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
